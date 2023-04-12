e3357c0488c0911ae81810951b902de279fd4756
Lukasz Gorka <lukaszgorka@hotmail.com>
2023-04-12 11:28:52 +0200

    add dtos and new endpoints for authors

--- src/authors/authors.controller.ts
+++ src/authors/authors.controller.ts
@@ -4,8 +4,14 @@ import {
   NotFoundException,
   ParseUUIDPipe,
   Param,
+  Post,
+  Body,
+  Put,
+  Delete,
 } from '@nestjs/common';
 import { AuthorsService } from './authors.service';
+import { CreateAuthorDTO } from './dtos/create-author.dto';
+import { UpdateAuthorDTO } from './dtos/update-author.dto';
 
 @Controller('authors')
 export class AuthorsController {
@@ -21,5 +27,29 @@ export class AuthorsController {
     const author = await this.authorsService.getById(id);
     if (!author) throw new NotFoundException('Author not found');
     return author;
+  }
+
+  @Post('/')
+  create(@Body() authorData: CreateAuthorDTO) {
+    return this.authorsService.create(authorData);
+  }
+
+  @Put('/:id')
+  async update(
+    @Param('id', new ParseUUIDPipe()) id: string,
+    @Body() authorData: UpdateAuthorDTO,
+  ) {
+    if (!(await this.authorsService.getById(id)))
+      throw new NotFoundException('Author not found');
+    await this.authorsService.updateById(id, authorData);
+    return { success: true };
+  }
+
+  @Delete('/:id')
+  async deleteById(@Param('id', new ParseUUIDPipe()) id: string) {
+    if (!(await this.authorsService.getById(id)))
+      throw new NotFoundException('Author not found');
+    await this.authorsService.deleteById(id);
+    return { success: true };
   }
 }
--- src/authors/authors.service.ts
+++ src/authors/authors.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ConflictException } from '@nestjs/common';
 import { PrismaService } from 'src/shared/services/prisma.service';
 import { Author } from '@prisma/client';
 
@@ -12,6 +12,40 @@ export class AuthorsService {
 
   public getById(id: Author['id']): Promise<Author | null> {
     return this.prismaService.author.findUnique({
+      where: { id },
+    });
+  }
+
+  public async create(authorData: Omit<Author, 'id'>): Promise<Author> {
+    try {
+      return await this.prismaService.author.create({
+        data: authorData,
+      });
+    } catch (error) {
+      if (error.code === 'P2002')
+        throw new ConflictException('Name is already taken');
+      throw error;
+    }
+  }
+
+  public async updateById(
+    id: Author['id'],
+    authorData: Omit<Author, 'id'>,
+  ): Promise<Author> {
+    try {
+      return await this.prismaService.author.update({
+        where: { id },
+        data: authorData,
+      });
+    } catch (error) {
+      if (error.code === 'P2002')
+        throw new ConflictException('Author not found');
+      throw error;
+    }
+  }
+
+  public deleteById(id: Author['id']): Promise<Author> {
+    return this.prismaService.author.delete({
       where: { id },
     });
   }
